4d790d9ae66a6ccc5d4bb9d768e129d5d101a4d0
Steven <ys2833@columbia.edu>
2017-08-17 21:26:37 -0400

    from old host

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* Scroll To */
+
+
+$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		$('html,body').animate({scrollTop: target.offset().top}, 600);
+	});
+});
